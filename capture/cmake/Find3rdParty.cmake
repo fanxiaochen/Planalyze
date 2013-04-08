57e9df7268798cf6fcffdaa56eae07017791cf58
yangyan.li <yangyan.li@hotmail.com>
2013-04-08 11:50:22 +0800

    remove serial port code from the project

--- capture/cmake/Find3rdParty.cmake
+++ capture/cmake/Find3rdParty.cmake
@@ -1,13 +1,5 @@
 set(ThirdParty_DIR ${PROJECT_SOURCE_DIR}/3rd-party)
 
-#qextserialport
-set(QExtSerialPort_INCLUDE_DIR ${ThirdParty_DIR}/qextserialport-1.2beta2/src)
-set(QExtSerialPort_LIBRARY_DIRS ${ThirdParty_DIR}/qextserialport-1.2beta2/lib)
-set(QExtSerialPort_LIBRARY optimized qextserialport-1.2.lib debug qextserialport-1.2d.lib )
-include_directories(${QExtSerialPort_INCLUDE_DIR})
-link_directories(${QExtSerialPort_LIBRARY_DIRS})
-set(ThirdParty_LIBS ${ThirdParty_LIBS} ${QExtSerialPort_LIBRARY})
-
 #FlyCapture2
 if (NOT FLYCAPTURE2_DIR)
   set(FLYCAPTURE2_DIR $ENV{FLYCAPTURE2_DIR})
