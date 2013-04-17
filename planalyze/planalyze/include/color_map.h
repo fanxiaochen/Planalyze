2c7751fd6c6761eeb357adb9e73858abe533c9a2
Xiaochen Fan <fanxiaochen.mmm@gmail.com>
2013-04-17 22:09:30 +0800

    enrich color_map

--- planalyze/planalyze/include/color_map.h
+++ planalyze/planalyze/include/color_map.h
@@ -14,11 +14,12 @@ public:
 
     typedef enum {JET} Map;
     typedef enum {LIGHT_BLUE} NamedColor;
+    typedef enum {DISCRETE_TOTAL, DISCRETE_KEY} DiscreteColor;
 
     /* more (non-static) functions here */
     const osg::Vec4& getColor(NamedColor nameColor);
     const osg::Vec4& getColor(Map map, float value, float low=0.0f, float high=1.0f);
-
+    const osg::Vec4& getColor(DiscreteColor map, float value, float low=0.0f, float high=1.0f);
     const osg::Vec4& getRandomColor() const;
 
 private:
@@ -29,6 +30,8 @@ private:
 
     std::vector<osg::Vec4> _jet;
     osg::Vec4              _lightBlue;
+    std::vector<osg::Vec4> _discrete_total;
+    std::vector<osg::Vec4> _discrete_key;
 
     const osg::Vec4& getColor(const std::vector<osg::Vec4>& map, float value, float low, float high);
 };
